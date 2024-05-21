6e63b8af40bff9859e741cd494b5584f827055a8
Gabriel Turetchi <gabriel.turetchi@egov.md>
2024-05-21 19:11:12 +0300

    Test changes

--- src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
+++ src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
@@ -2,12 +2,33 @@ package md.utm.cloudapp.rest
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
+
+
+/**
+ * Function for System Name
+ */
+fun getSystemName(): String? {
+    return try {
+        InetAddress.getLocalHost().hostName
+    } catch (E: Exception) {
+        System.err.println("System Name Exp : " + E.message)
+        null
+    }
+}
 
 @RestController
 class MainController {
 
     @GetMapping("/")
     fun main(): String {
-        return "Hello World!"
+        // Call Function to get Computer System Name
+        val systemName = getSystemName()
+
+        // Print Computer Name
+        println("Computer Name : $systemName")
+
+
+        return "Hello Prasada! $systemName"
     }
 }
